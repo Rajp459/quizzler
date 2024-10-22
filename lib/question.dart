f5bada58bd0da149f0ce09dd3b304677dc13b0fb
Raj Peajapari <prajapatiraj459@gmail.com>
2024-10-22 16:02:54 +0530

    Initial commit

---
+++ lib/question.dart
@@ -0,0 +1,9 @@
+class Question {
+  String questionText;
+  bool questionAnswer;
+
+  Question({required this.questionText, required this.questionAnswer});
+}
+
+Question newQuestion = Question(questionText: 'text', questionAnswer: true);
+// {required this.questionText,required this.questionAnswer}
